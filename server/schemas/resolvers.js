113c0e6e89b6df0a8d24e000d8a24f765d7b8423
DXHASE <danielchas886@gmail.com>
2022-12-07 21:16:30 -0600

    Began resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,18 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        me: async(parent, args,context) => {
+            if(context.user) {
+                data = await user.findOne({_id: context.user.id}).select('-__v -password');
+                return data;
+            }
+            throw new AuthenticationError('You need to be logged in!');
         }
     }
 }
+
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,31 @@ const { gql } = require('apollo-server-express');
 
 //My TypeDefs
 const typeDefs = gql`
+
+type User{
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Books]
+}
+
+type Books{
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
 type Query {
-    helloWorld: String
+    me: User
 }
 
 `;
